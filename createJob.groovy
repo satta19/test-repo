8c3b504448e6db7494831a948a49c3cce7dd7efd
Sourav Atta <atta19sourav@gmail.com>
2021-05-03 11:45:36 +0530

    Update createJob.groovy

--- createJob.groovy
+++ createJob.groovy
@@ -1,4 +1,18 @@
+import hudson.model.*
+ 
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
+
 def jobName = "ALM/" + new Random().with {(1..9).collect {(('a'..'z')).join()[ nextInt((('a'..'z')).join().length())]}.join()}
+
+def hardcoded_param = "name"
+def resolver = build.buildVariableResolver
+def hardcoded_param_value = resolver.resolve(hardcoded_param)
+ 
+ 
+println "param ${hardcoded_param} value : ${hardcoded_param_value}"
 
 
 pipelineJob("$jobName") {
@@ -8,7 +22,7 @@ pipelineJob("$jobName") {
   }
 
   parameters {
-         stringParam('name', 'john', 'name of the person')
+         stringParam('name', "${hardcoded_param_value}", 'name of the person')
     }
 
     definition {
