9f9b9b196e35859000c0b83bde86222a3b302c56
Sourav Atta <atta19sourav@gmail.com>
2021-07-01 10:41:05 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,10 +1,23 @@
+import hudson.model.*
+ 
+// get current thread / Executor
+def thr = Thread.currentThread()
+// get current build
+def build = thr?.executable
+
+
+
+def hardcoded_param = "name"
+def resolver = build.buildVariableResolver
+def hardcoded_param_value = resolver.resolve(hardcoded_param)
+
 pipelineJob("UAT Environment") {
   logRotator {
       numToKeep(5)
   }
 
   parameters {
-         stringParam('name', "", 'name of the person')
+         stringParam('name', "${hardcoded_param_value}", 'name of the person')
     }
 
     definition {
